bb56998f705edb68750cf0752ef0263c22b0012e
aladores <angelo.ladores@outlook.com>
2023-09-28 22:28:14 -0400

    initial commit, basic design and scroll snapping

---
+++ script.js
@@ -0,0 +1,18 @@
+// window.addEventListener("DOMContentLoaded", () => {
+//     //moveIssue();
+// });
+
+// function moveIssue() {
+
+//     window.onscroll = function () {
+//         let currUrl = window.location.href;
+//         if (this.oldScroll > this.scrollY) {
+//             window.location.href = "file:///Users/angeloladores/webdev/backstage-talks/index.html#issue-6"
+//         }
+
+//         else {
+//             console.log("going down");
+//         }
+//         this.oldScroll = this.scrollY;
+//     }
+// }
